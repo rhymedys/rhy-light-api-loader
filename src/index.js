2fa49c64d43ec11be7bbd622f8c553f01c23be9f
rhymedys <rhymedys@gmail.com>
2017-12-11 12:20:54 +0800

    change callback mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2017-11-29 11:41:02
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2017-11-29 17:36:08
+ * @Last Modified time: 2017-12-11 12:19:56
  */
 
 const utils = require('loader-utils')
@@ -10,14 +10,16 @@ module.exports = function (source, map, meta) {
   this.cacheable()
   let option = utils.getOptions(this) || {}
   let fileName = option.fileName && option.fileName.toLowerCase() || 'devapi'
-    if (map && map.sources[0].substring(map.sources[0].lastIndexOf('\\')+1,map.sources[0].lastIndexOf('.')).toLowerCase()===fileName) {
-      if (source.indexOf('exports.default') > -1) {
-        let removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
-        let removePartEndPostion = source.lastIndexOf('}')
-        let removePart = source.substring(removePartStartPostion + 1, removePartEndPostion)
-        let newSource = source.replace(removePart, '')
-        return newSource
-      }
+  if (map && map.sources[0].substring(map.sources[0].lastIndexOf('\\') + 1, map.sources[0].lastIndexOf('.')).toLowerCase() === fileName) {
+    if (source.indexOf('exports.default') > -1) {
+      let removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
+      let removePartEndPostion = source.lastIndexOf('}')
+      let removePart = source.substring(removePartStartPostion + 1, removePartEndPostion)
+      let newSource = source.replace(removePart, '')
+      this.callback(null, newSource, map, mata)
+      return
     }
-  return source
+  }
+  this.callback(null, source, map, mata)
+  return 
 }
