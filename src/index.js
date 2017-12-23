85e9af8827a6af47672a87bc954c8ae769114379
rhymedys <rhymedys@gmail.com>
2017-12-23 20:19:50 +0800

    fix resolve resource path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2017-11-29 11:41:02
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2017-12-11 12:25:49
+ * @Last Modified time: 2017-12-23 20:18:47
  */
 
 const utils = require('loader-utils')
@@ -10,7 +10,7 @@ module.exports = function (source, map, meta) {
   this.cacheable()
   let option = utils.getOptions(this) || {}
   let fileName = option.fileName && option.fileName.toLowerCase() || 'devapi'
-  if (map && map.sources[0].substring(map.sources[0].lastIndexOf('\\') + 1, map.sources[0].lastIndexOf('.')).toLowerCase() === fileName) {
+  if (this.resourcePath && this.resourcePath.substring(this.resourcePath.lastIndexOf('\\') + 1, this.resourcePath.lastIndexOf('.')).toLowerCase() === fileName) {
     if (source.indexOf('exports.default') > -1) {
       let removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
       let removePartEndPostion = source.lastIndexOf('}')
