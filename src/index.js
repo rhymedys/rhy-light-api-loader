8699ecfa732286cbcba5047df4e6bfe58144adaa
rhymedys <rhymedys@gmail.com>
2019-07-18 14:53:48 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2017-11-29 11:41:02
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2019-07-18 14:34:10
+ * @Last Modified time: 2019-07-18 14:53:29
  */
 
 const utils = require('loader-utils')
@@ -10,8 +10,8 @@ module.exports = function (source, map, meta) {
   this.cacheable()
   let option = utils.getOptions(this) || {}
   let fileName = option.fileName && option.fileName.toLowerCase() || 'devapi'
-  const lastFileNameStartPos = this.resourcePath.lastIndexOf('\\') + 1 || this.resourcePath.lastIndexOf('\/') +1
-  if (this.resourcePath && this.resourcePath.substring(lastFileNameStartPos, this.resourcePath.lastIndexOf('.')).toLowerCase() === fileName) {
+  const lastFileNameStartPos = this.resourcePath.lastIndexOf('\\') || this.resourcePath.lastIndexOf('\/') 
+  if (this.resourcePath && this.resourcePath.substring(lastFileNameStartPos + 1, this.resourcePath.lastIndexOf('.')).toLowerCase() === fileName) {
     if (source.indexOf('exports.default') > -1) {
       let removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
       let removePartEndPostion = source.lastIndexOf('}')
