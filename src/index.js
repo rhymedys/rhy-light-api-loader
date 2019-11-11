eb8989aa87a9585732f88f30c5ca587b7a396238
rhymedys <rhymedys@gmail.com>
2019-11-11 14:25:03 +0800

    调整filename api

--- src/index.js
+++ src/index.js
@@ -2,26 +2,36 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2017-11-29 11:41:02
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2019-07-18 15:15:05
+ * @Last Modified time: 2019-11-11 14:24:27
  */
 
 const utils = require('loader-utils')
+
 module.exports = function (source, map, meta) {
   this.cacheable()
-  let option = utils.getOptions(this) || {}
-  let fileName = option.fileName && option.fileName.toLowerCase() || 'devapi'
-  let lastFileSplitIndex = this.resourcePath.lastIndexOf('\\') + 1 || this.resourcePath.lastIndexOf('\/') + 1
-  if (this.resourcePath && this.resourcePath.substring(lastFileSplitIndex, this.resourcePath.lastIndexOf('.')).toLowerCase() === fileName) {
-    console.log('light-api-loader  match file',fileName)
+  const option = utils.getOptions(this) || {}
+
+  let fileNames = []
+
+
+  if (Array.isArray(option.fileName)) {
+    fileNames = option.fileName.map(val => val.toLowerCase())
+  } else {
+    fileNames[0] = option.fileName && option.fileName.toLowerCase()
+  }
+
+  const lastFileSplitIndex = this.resourcePath.lastIndexOf('\\') + 1 || this.resourcePath.lastIndexOf('\/') + 1
+  const fileName = this.resourcePath.substring(lastFileSplitIndex, this.resourcePath.lastIndexOf('.')).toLowerCase()
+  if (this.resourcePath && fileNames.indexOf(fileName) >= 0) {
+    console.log('light-api-loader  match file', fileName)
     if (source.indexOf('exports.default') > -1) {
-      let removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
-      let removePartEndPostion = source.lastIndexOf('}')
-      let removePart = source.substring(removePartStartPostion + 1, removePartEndPostion)
-      let newSource = source.replace(removePart, '')
+      const removePartStartPostion = source.indexOf('{', source.indexOf('exports.default'))
+      const removePartEndPostion = source.lastIndexOf('}')
+      const removePart = source.substring(removePartStartPostion + 1, removePartEndPostion)
+      const newSource = source.replace(removePart, '')
       this.callback(null, newSource, map, meta)
       return
     }
   }
   this.callback(null, source, map, meta)
-  return 
 }
